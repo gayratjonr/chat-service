3c786260d0eda91acb0395c0e974ce4fe6de22cf
g.rayimjonov <gayratjonr@gmial.com>
2024-05-14 15:08:25 +0500

    add logging

---
+++ src/main/java/uz/chat/service/aop/logging/LoggingAspect.java
@@ -0,0 +1,94 @@
+package uz.chat.service.aop.logging;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import uz.chat.service.config.ChatConstants;
+
+import java.util.Arrays;
+
+public class LoggingAspect {
+    private final Environment env;
+
+    public LoggingAspect(Environment env) {
+        this.env = env;
+    }
+
+    @Pointcut(
+            "within(@org.springframework.stereotype.Repository *)" +
+                    " || within(@org.springframework.stereotype.Service *)" +
+                    " || within(@org.springframework.web.bind.annotation.RestController *)"
+    )
+    public void springBeanPointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+    @Pointcut(
+            "within(uz.chat.service.repository..*)" + " || within(uz.chat.service.service..*)" + " || within(uz.chat.service.web.rest..*)"
+    )
+    public void applicationPackagePointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    /**
+     * Retrieves the {@link Logger} associated to the given {@link JoinPoint}.
+     *
+     * @param joinPoint join point we want the logger for.
+     * @return {@link Logger} associated to the given {@link JoinPoint}.
+     */
+    private Logger logger(JoinPoint joinPoint) {
+        return LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringTypeName());
+    }
+
+    @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        if (env.acceptsProfiles(Profiles.of(ChatConstants.SPRING_PROFILE_DEVELOPMENT))) {
+            logger(joinPoint)
+                    .error(
+                            "Exception in {}() with cause = '{}' and exception = '{}'",
+                            joinPoint.getSignature().getName(),
+                            e.getCause() != null ? e.getCause() : "NULL",
+                            e.getMessage(),
+                            e
+                    );
+        } else {
+            logger(joinPoint)
+                    .error(
+                            "Exception in {}() with cause = {}",
+                            joinPoint.getSignature().getName(),
+                            e.getCause() != null ? e.getCause() : "NULL"
+                    );
+        }
+    }
+    /**
+     * Advice that logs when a method is entered and exited.
+     *
+     * @param joinPoint join point for advice.
+     * @return result.
+     * @throws Throwable throws {@link IllegalArgumentException}.
+     */
+    @Around("applicationPackagePointcut() && springBeanPointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Logger log = logger(joinPoint);
+        if (log.isDebugEnabled()) {
+            log.debug("Enter: {}() with argument[s] = {}", joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+        }
+        try {
+            Object result = joinPoint.proceed();
+            if (log.isDebugEnabled()) {
+                log.debug("Exit: {}() with result = {}", joinPoint.getSignature().getName(), result);
+            }
+            return result;
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal argument: {} in {}()", Arrays.toString(joinPoint.getArgs()), joinPoint.getSignature().getName());
+            throw e;
+        }
+    }
+
+
+}
---
+++ src/main/java/uz/chat/service/config/ChatConstants.java
@@ -0,0 +1,10 @@
+package uz.chat.service.config;
+
+public interface ChatConstants {
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
+    String SPRING_PROFILE_LOCAL = "local";
+    /** Constant <code>SPRING_PROFILE_TEST="test"</code> */
+    String SPRING_PROFILE_TEST = "test";
+
+    String SPRING_PROFILE_PRODUCTION = "prod";
+}
